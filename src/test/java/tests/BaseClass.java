0216edb277dc7b80d5309d7e61e145886a52d75f
Ranajit Chandra <cranajit@amazon.com>
2019-12-12 16:29:23 +0530

    Added extent reporting

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -18,19 +18,19 @@ public class BaseClass extends ExtentReportsGeneration{
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setCapability(CapabilityType.PLATFORM_NAME, "Android");
         cap.setCapability(MobileCapabilityType.DEVICE_NAME, "OnePlus 5T");
-        cap.setCapability(MobileCapabilityType.UDID, "******");
+        cap.setCapability(MobileCapabilityType.UDID, "****");
         cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");
         cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "60");
         cap.setCapability(MobileCapabilityType.APP, "");
         cap.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
         cap.setCapability(MobileCapabilityType.BROWSER_VERSION, "74.0.3729");
 
-        //cap.setCapability(MobileCapabilityType.UDID, "192.168.14.242:5555");
 
         //TODO: FOR NATIVE APP USE THE BELOW MENTIONED CAPABILITIES
         //cap.setCapability("appPackage","com.oneplus.calculator");
         //cap.setCapability("appActivity", "com.oneplus.calculator.Calculator");
 
+        URL url = new URL("http://0.0.0.0:4723/wd/hub");
         driver = new AppiumDriver(url, cap);
     }
 
